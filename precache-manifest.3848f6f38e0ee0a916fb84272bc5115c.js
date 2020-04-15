c0645da79a7b052422835999117e515c48ac61f3
Fahad Israr <fahad00cms@gmail.com>
2020-04-16 02:08:47 +0530

    Updates

---
+++ precache-manifest.3848f6f38e0ee0a916fb84272bc5115c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9edf18e0c1b021391faf7f0ffa095de",
+    "url": "/invoice-generator/index.html"
+  },
+  {
+    "revision": "93954e3f0cbfa7a20eca",
+    "url": "/invoice-generator/static/css/3.e0e68d38.chunk.css"
+  },
+  {
+    "revision": "ec316971ba0768c7e128",
+    "url": "/invoice-generator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "54fa43159472b9513a4d",
+    "url": "/invoice-generator/static/js/2.272519e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/invoice-generator/static/js/2.272519e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93954e3f0cbfa7a20eca",
+    "url": "/invoice-generator/static/js/3.6b54e7bb.chunk.js"
+  },
+  {
+    "revision": "ec316971ba0768c7e128",
+    "url": "/invoice-generator/static/js/main.3294bb73.chunk.js"
+  },
+  {
+    "revision": "d82439447ee4d0193a4c",
+    "url": "/invoice-generator/static/js/runtime-main.ced41986.js"
+  }
+]);
